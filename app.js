7f1bed6663c53c7456aec5d219a1c0c2e1496c5f
nmahade <nmahade@iu.edu>
2022-12-11 00:33:26 -0500

    server issue

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join(__dirname,"images")));
 app.use(cors());
 
 
